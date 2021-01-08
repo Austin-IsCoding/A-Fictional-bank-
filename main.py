10f6db7c977c167fbe86103aaae9d2f875143225
Teerapat Pokaprakarn <teerapat@beyondcodeacademy.com>
2021-01-08 00:06:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Bill Splitting chatbot
+
+Splitting the cost of bills and expenses can be a clumsy process. You have to calculate who owed what, then go through the tedious task of collecting money from each person.
+
+
+
+Build a chatbot app that does the following.
+
+1. Let you divide up the cost of things like group dinners, group trips
+
+2. Inform each of your friend how much he/she owes
+'''
+
+your_chatbot_name = '.....'
+print("Hello my name is ")
